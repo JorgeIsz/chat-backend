94eea9ff3900042af360acba73337f00182b15ed
Jorge Isaza <isaza.jorge@hotmail.com>
2020-09-23 10:00:41 -0500

    Configuración de rooms

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
@@ -20,6 +20,32 @@ def index():
 
 mensajes = []
 
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    respuesta = {
+        'tipo':'alerta',
+        'mensaje':username + ' has entered the room ' + room
+    }
+    send(respuesta, room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
+
 @socketio.on('message')
 def handleMessage(msg):
     respuesta = {
@@ -27,7 +53,7 @@ def handleMessage(msg):
         'usuario':msg['usuario']
     }
     mensajes.append(respuesta)
-    emit('message', mensajes,broadcast=True)
+    emit('message', mensajes,broadcast=True,room="testroom")
 
 if __name__ == '__main__':
     socketio.run(app,host="localhost",debug=True)
